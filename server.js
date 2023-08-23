605df72cb009b492f49de362718e07078e0d6b61
Soumya mishra <soumya9589@gmail.com>
2023-08-23 11:56:31 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const cors = require("cors")
+const app = express();
+
+app.use(cors())
+
+const port = process.env.PORT || 4000;
+
+const apiData = require("./data.json")
+
+app.get('/', (req,res) => {
+    res.send(apiData)
+})
+app.listen(port, () => {
+    console.log(` server connected to port ${port}`)
+})
